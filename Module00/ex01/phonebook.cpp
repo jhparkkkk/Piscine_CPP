52b7acd16307191307797c071a3785bf7d7529ce
Jee hyun PARK <jeepark@e1r10p6.clusters.42paris.fr>
2022-11-07 17:46:12 +0100

    search

--- Module00/ex01/phonebook.cpp
+++ Module00/ex01/phonebook.cpp
@@ -3,21 +3,123 @@
 /*                                                        :::      ::::::::   */
 /*   phonebook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeepark <jeepark@student42.fr>             +#+  +:+       +#+        */
+/*   By: jeepark <jeepark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 23:14:56 by jeepark           #+#    #+#             */
-/*   Updated: 2022/11/01 23:43:50 by jeepark          ###   ########.fr       */
+/*   Updated: 2022/11/07 16:54:54 by jeepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <vector>
+#include <cstring>
+#include <algorithm>
 #include <iostream>
+
 #include "phonebook.hpp"
+
+Contact::Contact( void )
+{
+    std::cout << "Contact constructor." << std::endl;
+    return; 
+}
+
+Contact::~Contact( void )
+{
+    std::cout << "Contact destructor." << std::endl;
+    return; 
+}
+
+Phonebook::Phonebook( void )
+{
+    std::cout << "Phonebook constructor." << std::endl;
+    // Contact contact[8] = new Contact();
+    return;
+}
+
+Phonebook::~Phonebook( void )
+{
+    std::cout << "Phonebook destructor." << std::endl;
+    return; 
+}
+
+
+
+bool	is_phone_number(std::string data)
+{
+	
+}
+
+void    Phonebook::add(int idx)
+{
+    std::string data;
+    
+	std::cout << "add : " << idx << std::endl;
+    std::cout << "first name: " << std::endl;
+    std::cin >> data;
+	// if (cin.fail())
+    if (!data.empty())
+        this->contact[idx].first_name = data;
+    std::cout << "last name: " << std::endl;
+    std::cin >> data;
+    if (!data.empty())
+        this->contact[idx].last_name = data;
+    std::cout << "nickname: " << std::endl;
+    std::cin >> data;
+    if (!data.empty())
+        this->contact[idx].nickname = data;
+    std::cout << "phone number: " << std::endl;
+    std::cin >> data;
+    if (!data.empty())
+        this->contact[idx].phone_number = data;
+    std::cout << "darkest secret: " << std::endl;
+    std::cin >> data;
+    if (!data.empty())
+        this->contact[idx].darkest_secret = data;
+    return;
+}
+
+void	Phonebook::search()
+{
+	std::cout << "command search" << std::endl;
+	return;
+}
+
+
+
+
 
 int main(int ac, char **av)
 {
-    std::string str;
+    if (ac != 1)
+    {
+		std::cout << "Argument not permitted" << std::endl;
+		return (EXIT_FAILURE);
+	}
+    Phonebook phonebook = Phonebook();
+    std::string cmd;
+    int i = -1;
 
-    str = av[1];
-    std::cout << "plopd\n";
+    while (1)
+    {
+        std::cout << "Enter command: ";
+        std::cin >> cmd;
+        if (cmd == "ADD")
+        {
+			if (i == 7)
+				i = -1;
+            phonebook.add(++i);
+		}
+        else if (cmd == "SEARCH")
+			phonebook.search();
+		else
+            std::cout <<"Wrong command, try again: ";
+	}
+        // std::cout << "command ADD" << std::endl; 
+        // phonebook.contact[0].first_name = "jee";
+        // std::cout << phonebook.contact[1].first_name;
+        
     return 0;
 }
+
+// ctrl -D -> cin.eof a chercher
+// SEARCH : 10 carac a droite : methode existe deja
--- Module00/ex01/phonebook.hpp
+++ Module00/ex01/phonebook.hpp
@@ -3,33 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   phonebook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jeepark <jeepark@student42.fr>             +#+  +:+       +#+        */
+/*   By: jeepark <jeepark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 22:55:11 by jeepark           #+#    #+#             */
-/*   Updated: 2022/11/01 23:43:00 by jeepark          ###   ########.fr       */
+/*   Updated: 2022/11/07 16:14:04 by jeepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_H
 # define PHONEBOOK_H
 
+#include <vector>
+#include <cstring>
+#include <algorithm>
+#include <iostream>
+
 class Contact {
     public:
-    Contact( void );
-    ~Contact( void );
-    private:
     std::string first_name;
     std::string last_name;
     std::string nickname;
     std::string phone_number;
     std::string darkest_secret;  
+    Contact( void );
+    ~Contact( void );
+    // private:
+    
 };
 
 class Phonebook {
     public:
         Phonebook( void );
-        ~Phonebook( void);
-        Contact contact[8];            
+        ~Phonebook( void );
+        Contact contact[8];
+        void add(int idx);
+        void search();
+                  
 };
 
 
