a6369626dd7724b42574d71d8a26b786a90cc723
Jee hyun PARK <jeepark@e1r10p4.clusters.42paris.fr>
2022-11-08 18:13:11 +0100

    contact infos

--- Module00/ex01/phonebook.cpp
+++ Module00/ex01/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: jeepark <jeepark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 23:14:56 by jeepark           #+#    #+#             */
-/*   Updated: 2022/11/07 16:54:54 by jeepark          ###   ########.fr       */
+/*   Updated: 2022/11/08 18:07:50 by jeepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 #include <cstring>
 #include <algorithm>
 #include <iostream>
+#include <iomanip>
 
 #include "phonebook.hpp"
 
 Contact::Contact( void )
 {
     std::cout << "Contact constructor." << std::endl;
+    
     return; 
 }
 
@@ -46,7 +48,11 @@ Phonebook::~Phonebook( void )
 
 bool	is_phone_number(std::string data)
 {
-	
+    auto it = data.begin();
+    while (it != data.end() && std::isdigit(*it)) {
+        it++;
+    }
+    return !data.empty() && it == data.end();
 }
 
 void    Phonebook::add(int idx)
@@ -57,7 +63,7 @@ void    Phonebook::add(int idx)
     std::cout << "first name: " << std::endl;
     std::cin >> data;
 	// if (cin.fail())
-    if (!data.empty())
+    if (!data.empty() )
         this->contact[idx].first_name = data;
     std::cout << "last name: " << std::endl;
     std::cin >> data;
@@ -69,8 +75,12 @@ void    Phonebook::add(int idx)
         this->contact[idx].nickname = data;
     std::cout << "phone number: " << std::endl;
     std::cin >> data;
-    if (!data.empty())
-        this->contact[idx].phone_number = data;
+    if (!is_phone_number(data))
+    {
+        std::cout << "Invalid phone number" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    this->contact[idx].phone_number = data;
     std::cout << "darkest secret: " << std::endl;
     std::cin >> data;
     if (!data.empty())
@@ -78,10 +88,56 @@ void    Phonebook::add(int idx)
     return;
 }
 
-void	Phonebook::search()
+std::string    set_space(std::string arg)
 {
-	std::cout << "command search" << std::endl;
-	return;
+    std::string space;
+
+    space = string(10 - arg.size());
+    return space;
+}
+
+/*
+    .....index|first name|.last name|..nickname
+    .........0|.......jee|......park|.....jeejo
+
+*/
+void    display_contact(std::string arg1, std::string arg2, std::string arg3, std::string arg4)
+{
+    arg1.resize(10);
+    arg2.resize(10);
+    arg3.resize(10);
+    arg4.resize(10);
+
+    const std::string sep = "|" ;
+    const std::string space = " ";
+    std::cout << std::right << set_space(2, " ")  << arg1 << sep
+            << std::right << set_space(2, " ")  << arg2 << sep
+            << std::right <<  set_space(2, " ") << arg3 << sep
+            << std::right <<  set_space(2, " ") << arg4 << '\n';
+    // std::cout << std::right << std::setw(10) << std::string(arg1.length() - 10, 32)  << arg1 << sep
+    //         << std::right << std::setw(10)<< std::string(arg2.length() - 10, 32)   << arg2 << sep
+    //         << std::right << std::setw(10) << arg3 << sep
+    //         << std::right << std::setw(10) << arg4 << '\n';
+    
+}
+
+void	Phonebook::search(int i)
+{
+    int count = 0;
+    std::string index;
+    display_contact("index", "first name", "last name", "nickname");
+    while (count <= i)
+    {
+        if (this->contact->first_name.empty())
+            return;
+        display_contact(std::to_string(count), this->contact[count].first_name,
+        this->contact[count].last_name, this->contact[i].nickname);
+        count++;
+    }
+    std::cout << "enter index for more information: ";
+	std::cin >> index;
+    
+    return;
 }
 
 
@@ -98,8 +154,8 @@ int main(int ac, char **av)
     Phonebook phonebook = Phonebook();
     std::string cmd;
     int i = -1;
-
-    while (1)
+    
+    while (!std::cin.eof())
     {
         std::cout << "Enter command: ";
         std::cin >> cmd;
@@ -110,9 +166,12 @@ int main(int ac, char **av)
             phonebook.add(++i);
 		}
         else if (cmd == "SEARCH")
-			phonebook.search();
+		{
+            // std::cout << i;
+            phonebook.search(i);
+        }
 		else
-            std::cout <<"Wrong command, try again: ";
+            std::cout <<"Wrong command, try again: " << std::endl;
 	}
         // std::cout << "command ADD" << std::endl; 
         // phonebook.contact[0].first_name = "jee";
@@ -121,5 +180,6 @@ int main(int ac, char **av)
     return 0;
 }
 
+// check phone number 
 // ctrl -D -> cin.eof a chercher
-// SEARCH : 10 carac a droite : methode existe deja
+// SEARCH : 10 carac a droite : methode existe deja str.resize 
--- Module00/ex01/phonebook.hpp
+++ Module00/ex01/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: jeepark <jeepark@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 22:55:11 by jeepark           #+#    #+#             */
-/*   Updated: 2022/11/07 16:14:04 by jeepark          ###   ########.fr       */
+/*   Updated: 2022/11/08 18:09:54 by jeepark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,35 @@
 #include <algorithm>
 #include <iostream>
 
+// info contact dans prive 
+
 class Contact {
     public:
+    Contact( );
+    ~Contact( void );
+    std::string getFirstName();
+    std::string getLastName();
+    std::string getNickname();
+    std::string getPhoneNumber();
+    std::string getFirstName();
+    
+    private:
     std::string first_name;
     std::string last_name;
     std::string nickname;
     std::string phone_number;
     std::string darkest_secret;  
-    Contact( void );
-    ~Contact( void );
-    // private:
-    
 };
 
 class Phonebook {
     public:
         Phonebook( void );
         ~Phonebook( void );
-        Contact contact[8];
         void add(int idx);
-        void search();
-                  
+        void search(int i);
+    private:
+        Contact contact[8];
+            
 };
 
 
